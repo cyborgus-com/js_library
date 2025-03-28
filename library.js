e4db16a026fbe6537339ee05b6c6d5da54b4185b
karolisurbonas <karolis.urbonas@gmail.com>
2025-03-28 12:38:54 -1000

    added form, and some basic JS, still not functional

--- library.js
+++ library.js
@@ -1,9 +1,25 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(id, author, title, pages, genre, read) {
+    // the constructor...
+
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.genre = genre;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(author, title, pages, genre, read="No") {
+    // take params, create a book then store it in the array
+
+    let id = self.crypto.randomUUID();
+
+    const book_temp = new Book(id, author, title, pages, genre, read);
+    myLibrary.push(book_temp);
+}
+
+function displayBooks(arr) {
+
 }
